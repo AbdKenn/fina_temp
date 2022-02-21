2b79ff4802c4ed6883db2cfcc5f1a73591760bdf
AbdKenn <99841144+AbdKenn@users.noreply.github.com>
2022-02-21 01:21:57 +0100

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from onlinecourse import views
 
 urlpatterns = [
     path('', views.home, name='home'),
